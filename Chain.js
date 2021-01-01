aa9b585efbefe3db0fb4fc0bf606a725ff59cb40
leenav3 <65767815+leenav3@users.noreply.github.com>
2021-01-01 15:52:05 +0530

    Update Chain.js
    Chain.js is modified for interchanging body1 with bob and body2 with roof to avoid unexpected movements of the bobs.

--- Chain.js
+++ Chain.js
@@ -1,13 +1,13 @@
 class Chain{
-    constructor(body1,body2,offsetX,offsetY){
+    constructor(body1,body2,offsetX,offsetY){  //bob is passed for body1 and roof is passed for body2 from sketch.js
 
         this.offsetX=offsetX;
         this.offsetY=offsetY
         var options = {
-            bodyA: body1,
+            bodyA: body1,        
             bodyB:body2,
-            stiffness: 0.04,
-            length: 10,
+           // stiffness: 0.04,
+           // length: 10,
             pointB:{x:this.offsetX,y:this.offsetY}
     
         }
@@ -18,16 +18,21 @@ class Chain{
     
     display(){
 
-        var pointX=this.chain.bodyA.position;
-        var pointY=this.chain.bodyB.position;
+        var pointX=this.chain.bodyA.position;  //bob's position
+        var pointY=this.chain.bodyB.position;  //roof's position
         
         
         strokeWeight(5);
         fill("black");
-        var attactPointX= pointX.x+ this.offsetX;
-        var attachPointY=pointX.y+ this.offsetY;
+      //  var attactPointX= pointX.x+ this.offsetX;
+      //  var attachPointY=pointX.y+ this.offsetY;
 
-        line(attactPointX,attachPointY,pointY.x,pointY.y)
+      //  line(attactPointX,attachPointY,pointY.x,pointY.y)
+        var attactPointX= pointY.x+ this.offsetX; //add displacements to the roof position
+        var attachPointY=pointY.y+ this.offsetY;
+
+        line(pointX.x,pointX.y,attactPointX,attachPointY)  //line(bob's position, roof's position)
+
 
         
 
